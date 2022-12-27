704437292e27022b76f30e6b01814c46966bb37a
JavierMarSan18 <marroquinjavier75@gmail.com>
2022-12-27 17:30:58 -0600

    Se agrego la funcionalidad de encriptar, desencriptar y copiar al clipboard

--- js/encryption.js
+++ js/encryption.js
@@ -1,6 +1,3 @@
-let btnEnc = document.getElementById("btn-enc");
-let btnDes = document.getElementById("btn-des");
-let btnCopy = document.getElementById("btn-copy");
 let words = ["ai", "enter", "imes", "ober", "ufat"];
 let vowels = ["a", "e", "i", "o", "u"];
 
@@ -8,13 +5,11 @@ let vowels = ["a", "e", "i", "o", "u"];
 let encrypt = (word) => {
   let encryptedWord = "";
 
-  if (!isLowerCase(word)) {
-    for (let i = 0; i < word.length; i++) {
-      if (vowels.includes(word.charAt(i))) {
-        encryptedWord += words[vowels.indexOf(word.charAt(i))];
-      } else {
-        encryptedWord += word.charAt(i);
-      }
+  for (let i = 0; i < word.length; i++) {
+    if (vowels.includes(word.charAt(i))) {
+      encryptedWord += words[vowels.indexOf(word.charAt(i))];
+    } else {
+      encryptedWord += word.charAt(i);
     }
   }
 
@@ -25,56 +20,10 @@ let encrypt = (word) => {
 let decrypt = (word) => {
   let decryptedWord = "";
 
-  if (!isLowerCase(word)) {
-    for (let i = 0; i < words.length; i++) {
-      word = word.replaceAll(words[i], vowels[i]);
-      decryptedWord = word;
-    }
+  for (let i = 0; i < words.length; i++) {
+    word = word.replaceAll(words[i], vowels[i]);
+    decryptedWord = word;
   }
 
   return decryptedWord;
 };
-
-//Verifica si la cadena ingresada contiene mayusculas
-let isLowerCase = (word) => {
-  return word.toLowerCase() != word;
-};
-
-//Devuelve el valor que contiene el input para ingresar el texto
-let getWordFromInputText = () => {
-  let inputText = document.getElementById("input-text");
-  return inputText.value;
-};
-
-//Devuelve el valor que contiene el input donde el texto es mostrado
-let getWordFromOutputText = () => {
-    let ouputText = document.getElementById("output-text");
-    return ouputText.value;
-  };
-
-//Carga la cadena al input de salida de texto
-let loadWordToOutputText = (word) => {
-  let outputText = document.getElementById("output-text");
-  outputText.value = word;
-};
-
-//Carga la cadena encriptada al input de salida de texto
-let loadEncryptedWord = () => {
-  let word = getWordFromInputText();
-  loadWordToOutputText(encrypt(word));
-};
-
-//Carga la cadena desencriptada al input de salida de texto
-let loadDecryptedWord = () => {
-  let word = getWordFromInputText();
-  loadWordToOutputText(decrypt(word));
-};
-
-//Copia la cadena del input de salida hacia el clipboard.
-let copyToClipboard = () => {
-  navigator.clipboard.writeText(getWordFromOutputText());
-};
-
-btnEnc.onclick = loadEncryptedWord;
-btnDes.onclick = loadDecryptedWord;
-btnCopy.onclick = copyToClipboard;
--- js/main.js
+++ js/main.js
@@ -1,1 +1,68 @@
+let btnEncrypt = document.getElementById('btn-encrypt');
+let btnDecrypt = document.getElementById('btn-decrypt')
+let btnCopy = document.getElementById('btn-copy');
 
+//Cambia el display de los contenedores de salida
+let toggleOutputContainer = () => {
+    let messageContainer = document.getElementById('message-container');
+    messageContainer.style.display = 'none'
+
+    let outputTextContainer = document.getElementById('output-text-container');
+    outputTextContainer.style.display = 'flex';
+}
+
+//Verifica si la cadena ingresada contiene mayusculas
+let isLowerCase = (word) => {
+    return word.toLowerCase() == word;
+};
+
+//Devuelve el valor que contiene el input para ingresar el texto
+let getWordFromInputText = () => {
+    let inputText = document.getElementById("input-text");
+    return inputText.value;
+  };
+  
+
+//Devuelve el valor que contiene el input donde el texto es mostrado
+let getWordFromOutputText = () => {
+    let ouputText = document.getElementById("output-text");
+    return ouputText.value;
+};
+
+//Copia la cadena del input de salida hacia el clipboard.
+let copyToClipboard = () => {
+    navigator.clipboard.writeText(getWordFromOutputText());
+  };
+
+  //Carga la cadena al input de salida de texto
+let loadWordToOutputText = (word) => {
+    let outputText = document.getElementById("output-text");
+    outputText.value = word;
+};
+
+
+//Se agregan eventos a los botones
+//Encripta la cadena y la carga al contenedor de salida
+btnEncrypt.addEventListener('click', () => {
+    let word = getWordFromInputText();
+
+    if(isLowerCase(word)){
+        toggleOutputContainer();
+        let encryptedWord = encrypt(word);
+        loadWordToOutputText(encryptedWord);
+    }
+});
+
+//Desencripta la cadena y la carga al contenedor de salida
+btnDecrypt.addEventListener('click', () => {
+    let word = getWordFromInputText();
+
+    if(isLowerCase(word)){
+        toggleOutputContainer();
+        let decryptedWord = decrypt(word);
+        loadWordToOutputText(decryptedWord);
+    }
+})
+
+//Copia la cadena al clipboard
+btnCopy.onclick = copyToClipboard;
