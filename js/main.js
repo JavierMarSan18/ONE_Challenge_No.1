a294ebb75a5e78ad923ef7937b1cc931af097ca2
JavierMarSan18 <marroquinjavier75@gmail.com>
2022-12-27 23:32:33 -0600

    Se agrego animacion al warning y se optimizo la validacion de la cadena

--- js/main.js
+++ js/main.js
@@ -11,10 +11,10 @@ let toggleOutputContainer = () => {
     outputTextContainer.style.display = 'flex';
 }
 
-//Verifica si la cadena ingresada contiene mayusculas
-let isLowerCase = (word) => {
-    return word.toLowerCase() == word;
-};
+//Valida que la cadena solo contenga letras minúsculas y espacios.
+let validateString = (str) => {
+    return !str.match(/[^a-zñ ]/);
+}
 
 //Devuelve el valor que contiene el input para ingresar el texto
 let getWordFromInputText = () => {
@@ -22,11 +22,16 @@ let getWordFromInputText = () => {
     return inputText.value;
   };
   
-
 //Devuelve el valor que contiene el input donde el texto es mostrado
 let getWordFromOutputText = () => {
     let ouputText = document.getElementById("output-text");
     return ouputText.value;
+};
+
+//Carga la cadena al input de salida de texto
+let loadWordToOutputText = (str) => {
+  let outputText = document.getElementById("output-text");
+  outputText.value = str;
 };
 
 //Copia la cadena del input de salida hacia el clipboard.
@@ -34,23 +39,36 @@ let copyToClipboard = () => {
     navigator.clipboard.writeText(getWordFromOutputText());
   };
 
-  //Carga la cadena al input de salida de texto
-let loadWordToOutputText = (word) => {
-    let outputText = document.getElementById("output-text");
-    outputText.value = word;
-};
-
-
+//Aplica las validaciones en la cadena para poder ser encriptada o desencriptada
 let processWord = (action) => {
-    let word = getWordFromInputText();
+    let str = getWordFromInputText();
 
-    if(word != ""  && isLowerCase(word)){
+    if(str == ""){
+        return;
+    }
+
+    if(validateString(str)){
         toggleOutputContainer();
-        let proccessedWord = action(word);
-        loadWordToOutputText(proccessedWord);
+        let processedStr = action(str);
+        loadWordToOutputText(processedStr);
+    }else{
+        animateWarningContainer();
     }
 }
 
+let animateWarningContainer = () => {
+    let warningContainer = document.getElementById('warning-container');
+    warningContainer.style = `
+        transition-duration: 0.3s;
+        padding-left: 2%;
+    `;
+
+    setTimeout(() => {
+        warningContainer.style = `
+        transition-duration: 0.3s;
+    `;
+    }, 300);
+}
 
 //Se agregan eventos a los botones
 //Encripta la cadena y la carga al contenedor de salida
